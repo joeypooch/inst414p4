aa86ec7ad111814cfd2b1b48d3662594f930e29a
joeypooch <58953076+joeypooch@users.noreply.github.com>
2023-05-11 18:27:11 -0400

    Update inst414p4.py

--- inst414p4.py
+++ inst414p4.py
@@ -10,38 +10,34 @@ from scipy.spatial.distance import pdist, squareform
 
 # API Key for News API
 api_key = "1879c06fe71c467c8b39cb6b7255af19"
-
-# Define API endpoint and parameters
 url = ('http://newsapi.org/v2/everything?'
        'q=ukraine&'
        'sortBy=popularity&'
        'pageSize=100&'
        f'apiKey={api_key}')
-
-# Send request and get response
 response = requests.get(url)
 data = response.json()
 
-# Save data to file
+
 with open('ukraine_articles.json', 'w') as f:
     json.dump(data, f)
 
-# Load data from file
+
 with open('ukraine_articles.json', 'r') as f:
     data = json.load(f)
 
 # Extract article titles and descriptions
 articles = [(article['title'] + '. ' + article['description']) for article in data['articles']]
 
-# Vectorize articles using TF-IDF
+# Vectorize 
 vectorizer = TfidfVectorizer(stop_words='english')
 X = vectorizer.fit_transform(articles)
 
-# Reduce dimensionality with PCA
+# Reduce dimensionality 
 pca = PCA(n_components=2)
 X_pca = pca.fit_transform(X.toarray())
 
-# Determine optimal number of clusters using elbow method
+# Clusters
 distortions = []
 K = range(1, 10)
 
@@ -61,29 +57,29 @@ plt.title('Elbow Method for Optimal k')
 plt.show()
 
 
-# Cluster articles using Spectral Clustering
+# Spectral Clustering
 clustering = SpectralClustering(n_clusters=3, assign_labels="discretize", random_state=42).fit(X)
 
-# Add cluster labels to PCA data
+
 df = pd.DataFrame(X_pca, columns=['x', 'y'])
 df['label'] = clustering.labels_
 
-# Generate network graph of articles and their similarity
+
 dist = pdist(X.toarray(), metric='cosine')
 dist = 1 - squareform(dist)
 G = nx.from_numpy_array(dist)
 
-# Add article titles as node attributes
+
 mapping = {}
 for i, article in enumerate(data['articles']):
     mapping[i] = article['title']
 nx.relabel_nodes(G, mapping, copy=False)
 
-# Add cluster labels as node attributes
+
 for node in G.nodes:
     G.nodes[node]['cluster'] = clustering.labels_[i]
 
-# Print clusters
+
 for i in range(clustering.n_clusters):
     print(f'Cluster {i}:')
     for j, article in enumerate(data['articles']):
@@ -95,7 +91,7 @@ for i in range(clustering.n_clusters):
 sizes = [len(G.edges(node)) for node in G.nodes]
 nx.set_node_attributes(G, dict(zip(G.nodes, sizes)), 'size')
 
-# Plot network graph
+# Graph
 pos = nx.spring_layout(G, k=0.25)
 node_colors = clustering.labels_
 cmap = plt.cm.get_cmap('viridis', len(set(node_colors)))
